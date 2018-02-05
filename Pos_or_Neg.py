328889ada417e9e35b0c2f6eaccf3103d7be6f0d
Hemapriya485 <36149680+Hemapriya485@users.noreply.github.com>
2018-02-05 15:26:20 +0530

    Add files via upload

---
+++ Pos_or_Neg.py
@@ -0,0 +1,24 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      kalpana
+#
+# Created:     05/02/2018
+# Copyright:   (c) kalpana 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+a=int(input())
+if(a>0):
+    print("No is positive")
+elif(a<0):
+    print("No is negative")
+else:
+    print("Zero")
+
