2c9db39e631734fece18d7e0f898b7b6278fcc3b
Hemapriya485 <36149680+Hemapriya485@users.noreply.github.com>
2018-02-05 16:33:42 +0530

    Update Vowel_or_Cons.py

--- Vowel_or_Cons.py
+++ Vowel_or_Cons.py
@@ -1,20 +1,3 @@
-#-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
-#
-# Author:      kalpana
-#
-# Created:     05/02/2018
-# Copyright:   (c) kalpana 2018
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
-
-def main():
-    pass
-
-if __name__ == '__main__':
-    main()
-
 import string
 alpha_letters=[]
 alpha_letters=string.ascii_letters
