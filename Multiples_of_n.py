cbb276b83b94212251259005b0fb7161d9b9b20a
Hemapriya485 <36149680+Hemapriya485@users.noreply.github.com>
2018-02-07 13:45:39 +0530

    Add files via upload

---
+++ Multiples_of_n.py
@@ -0,0 +1,10 @@
+def multiple(n):
+    my_list=[]
+    for x in range(1,6):
+        my_list.append(n*x)
+    return my_list
+print("Enter a number:")
+n=int(input())
+print(n)
+print("First five multiples of "+str(n))
+print(*multiple(n))
